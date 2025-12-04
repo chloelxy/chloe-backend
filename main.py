4808a74d37e85346c5944996326737bc7a6c6326
chloe <chloelxy0911@gmail.com>
2025-12-04 15:49:08 +0800

    update main

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import cohere
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class chatRequest(BaseModel):
+    prompt: str
+
+class chatResponse(BaseModel):
+    response: str
 
 app = FastAPI()
+cohere_client = cohere.ClientV2(api_key=os.getenv("COHERE_API_KEY"))
 
 @app.get("/")
-def health_check():
-    return {"status": "ok"}
+def calculation():
+    answer = 10 + 10
+    return {"answer": answer}
+
+@app.post("/chat", response_model=chatResponse)
+def chat(request: chatRequest):
+    response = cohere_client.chat(
+        model="command-a-03-2025",
+        messages=[
+            {
+                "role": "user",
+                "content": request.prompt
+            }
+        ]
+    )
+
+    print(response)
+    return chatResponse(response=f"Hi, how are you!")
